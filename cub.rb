84a8efa358d05752c8ac09d283edf06293546bcb
Daniel Agans <daniel.agans@llamasoft.com>
2018-10-17 22:51:05 -0400

    initial commit'

---
+++ cub.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/ruby
+
+# Chef Upload Bump
+
+require 'io/console'
+require 'stringio'
+
+def arg_logic
+  ARGV[0] || parse_cbname
+end
+
+# This is only going to extract the name if its first instance of name
+# because that if statement for some reason gets shit from the middle
+# of a line, so if "dirname" is in there it's matching. DIY for later
+def parse_cbname
+  file = 'metadata.rb'
+  verify_file("./#{file}")
+  IO.readlines("./#{file}", 'r').each do |line|
+    # checks the first 6 characters of each line for 'name{space}{single quote}'
+    if line[0..5] == 'name \''
+      # gets the name from returned string and returns that as string
+      return line.match(/(["'])(?:(?=(\\?))\2.)*?\1/).to_s.delete('\'')
+    end
+  end
+end
+
+# Check if the file (metadata) exists
+def verify_file(filename)
+  if File.file?("./#{filename}")
+    true
+  else
+    puts 'Usage: ruby cub.rb cookbookname'
+  end
+end
+
+cookbook = arg_logic
+# Main
+`knife spork bump #{cookbook}`
+`knife cookbook upload #{cookbook}`
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'winlogbeat'
+maintainer 'Azat Khadiev'
+maintainer_email 'anuriq@gmail.com'
+license 'Apache 2.0'
+description 'Installs/Configures Elastic Winlogbeat'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url 'https://github.com/anuriq/chef-winlogbeat' if respond_to?(:source_url)
+issues_url 'https://github.com/anuriq/chef-winlogbeat/issues' if respond_to?(:issues_url)
+version '1.0.4'
+
+supports 'windows'
+
+chef_version '>= 12' if respond_to?(:chef_version)
+
+
