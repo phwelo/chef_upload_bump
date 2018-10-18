cbd292eb4c454417e243c9652b2f8dcd8e8cfe3d
Daniel Agans <daniel.agans@llamasoft.com>
2018-10-17 23:56:30 -0400

    IO open caused issues as it didnt get line breaks right

--- cub.rb
+++ cub.rb
@@ -2,22 +2,17 @@
 
 # Chef Upload Bump
 
-require 'io/console'
-
 def arg_logic
   ARGV[0] || parse_cbname
 end
 
-# This is only going to extract the name if its first instance of name
-# because that if statement for some reason gets shit from the middle
-# of a line, so if "dirname" is in there it's matching. DIY for later
+# DIY - figure out why this seems to match things that don't start with name...
 def parse_cbname
   file = 'metadata.rb'
   verify_file("./#{file}")
-  IO.readlines("./#{file}", 'r').each do |line|
-    # checks the first 6 characters of each line for 'name{space}{single quote}'
-    if line[0..5] == 'name \''
-      # gets the name from returned string and returns that as string
+  metadata = File.readlines "./#{file}"
+  metadata.each do |line|
+    if line.start_with?('name')
       return line.match(/(["'])(?:(?=(\\?))\2.)*?\1/).to_s.delete('\'')
     end
   end
@@ -29,10 +24,11 @@ def verify_file(filename)
     true
   else
     puts 'Usage: / ruby cub.rb cookbookname'
+    exit
   end
 end
 
 cookbook = arg_logic
 # Main
-`knife spork bump #{cookbook}`
-`knife cookbook upload #{cookbook}`
+system("knife spork bump #{cookbook}")
+system("knife cookbook upload #{cookbook}")
